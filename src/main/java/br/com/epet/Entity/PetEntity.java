47be33399c3dfb5fde74d1c6767aca2d2ea2573f
BernardoWitkoski <bernardowitkoski@hotmail.com>
2022-12-16 23:13:43 -0300

    Creating Pet class

---
+++ src/main/java/br/com/epet/Entity/PetEntity.java
@@ -0,0 +1,18 @@
+package br.com.epet.Entity;
+
+import lombok.Data;
+
+@Data
+public class PetEntity {
+
+    long id;
+
+    String name;
+
+    // specie
+
+    private double weight;
+
+    
+
+}
