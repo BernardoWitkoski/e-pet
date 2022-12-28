9c3ba5877179e59d45aaa477534a863ff67452cb
BernardoWitkoski <bernardowitkoski@hotmail.com>
2022-12-28 00:30:06 -0300

    Defining Owner request URL

--- src/main/java/br/com/epet/Controller/OwnerController.java
+++ src/main/java/br/com/epet/Controller/OwnerController.java
@@ -1,4 +1,12 @@
 package br.com.epet.Controller;
 
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v1/owner")
 public class OwnerController {
+
+    
 }
