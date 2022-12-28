3767babd2b76e559ca787773ef3b858eff3acb3a
BernardoWitkoski <bernardowitkoski@hotmail.com>
2022-12-28 00:42:27 -0300

    Mongo repository to Owner

--- src/main/java/br/com/epet/Repository/OwnerRepository.java
+++ src/main/java/br/com/epet/Repository/OwnerRepository.java
@@ -1,4 +1,8 @@
 package br.com.epet.Repository;
 
-public interface OwnerRepository {
+import br.com.epet.Entity.OwnerEntity;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface OwnerRepository extends MongoRepository<OwnerEntity, Long> {
+
 }
