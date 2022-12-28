e0ea895bd51b1e9fdfcfdaaa27375100529ebb1a
BernardoWitkoski <bernardowitkoski@hotmail.com>
2022-12-28 00:31:53 -0300

    Defining Pet request URL

--- src/main/java/br/com/epet/Controller/PetController.java
+++ src/main/java/br/com/epet/Controller/PetController.java
@@ -1,4 +1,11 @@
 package br.com.epet.Controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v1/pets")
 public class PetController {
+
+
 }
