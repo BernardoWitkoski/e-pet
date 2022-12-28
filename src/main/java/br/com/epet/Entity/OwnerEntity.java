6cb46f638140f7f40b1cc6ba37dea8b46ca55996
BernardoWitkoski <bernardowitkoski@hotmail.com>
2022-12-28 00:09:26 -0300

    Mapping Owner collection

--- src/main/java/br/com/epet/Entity/OwnerEntity.java
+++ src/main/java/br/com/epet/Entity/OwnerEntity.java
@@ -1,10 +1,12 @@
 package br.com.epet.Entity;
 
 import lombok.Data;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.List;
 
 @Data
+@Document("owner")
 public class OwnerEntity {
 
     private Long id;
