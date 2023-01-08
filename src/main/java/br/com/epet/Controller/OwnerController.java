5436f1cac3e2fe263bba951504f0330b799684ca
BernardoWitkoski <bernardowitkoski@hotmail.com>
2023-01-07 23:00:39 -0300

    Owner service methods to Controller

--- src/main/java/br/com/epet/Controller/OwnerController.java
+++ src/main/java/br/com/epet/Controller/OwnerController.java
@@ -1,5 +1,7 @@
 package br.com.epet.Controller;
 
+import br.com.epet.Service.OwnerService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,6 +9,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/v1/owner")
 public class OwnerController {
+
+    @Autowired
+    private OwnerService service;
 
     
 }
