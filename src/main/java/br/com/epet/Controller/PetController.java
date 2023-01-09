e7dcb8202734fb6565fffcabb60d575b635105a9
BernardoWitkoski <bernardowitkoski@hotmail.com>
2023-01-08 22:41:08 -0300

    Post Pet method

--- src/main/java/br/com/epet/Controller/PetController.java
+++ src/main/java/br/com/epet/Controller/PetController.java
@@ -1,7 +1,13 @@
 package br.com.epet.Controller;
 
+import br.com.epet.Dto.PetDTO;
+import br.com.epet.Dto.PetFormDTO;
 import br.com.epet.Service.PetService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,4 +18,9 @@ public class PetController {
     @Autowired
     private PetService service;
 
+    @PostMapping()
+    public ResponseEntity<PetDTO> save(@RequestBody PetFormDTO body) {
+        PetDTO pet = this.service.save(body);
+        return ResponseEntity.status(HttpStatus.CREATED).body(pet);
+    }
 }
