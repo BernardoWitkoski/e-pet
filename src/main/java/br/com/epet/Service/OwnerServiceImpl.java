a6ab021ca9b4a76e9c7ab7394dce81a41a5e7134
BernardoWitkoski <bernardowitkoski@hotmail.com>
2023-01-12 23:25:39 -0300

    Implementing service interface and it's methods

--- src/main/java/br/com/epet/Service/OwnerServiceImpl.java
+++ src/main/java/br/com/epet/Service/OwnerServiceImpl.java
@@ -1,6 +1,12 @@
 package br.com.epet.Service;
 
-public class OwnerServiceImpl {
+import br.com.epet.Dto.OwnerDTO;
 
-    
+public class OwnerServiceImpl implements OwnerService {
+
+
+    @Override
+    public OwnerDTO create(OwnerDTO owner) {
+        return null;
+    }
 }
